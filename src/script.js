6164ed8b0ce9fab68761cc43b50efa8b3bb708ac
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2023-12-30 17:51:19 +0000

    display time of location

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
+function updateTime() {
+  //SEOUL
+  let seoulElement = document.querySelector("#seoul_info");
+  let seoulDateElement = seoulElement.querySelector(".date");
+  let seoulTimeElement = seoulElement.querySelector(" h3");
+  let seoulTime = moment().tz("Asia/Seoul");
+  seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<span>]a[</span>]");
 
+  // PRAGUE
+  let pragueElement = document.querySelector("#prague_info");
+  let pragueDateElement = pragueElement.querySelector(".date");
+  let pragueTimeElement = pragueElement.querySelector(" h3");
+  let pragueTime = moment().tz("Europe/Prague");
+  pragueDateElement.innerHTML = pragueTime.format("MMMM Do YYYY");
+  pragueTimeElement.innerHTML = pragueTime.format("h:mm:ss [<span>]a[</span>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
