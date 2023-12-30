f2d10180760180faf7c72c0046df1573b3573016
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2023-12-30 22:27:03 +0000

    change citys and added function to other cities time

--- src/script.js
+++ src/script.js
@@ -1,19 +1,45 @@
 function updateTime() {
   //SEOUL
   let seoulElement = document.querySelector("#seoul_info");
-  let seoulDateElement = seoulElement.querySelector(".date");
-  let seoulTimeElement = seoulElement.querySelector(" h3");
-  let seoulTime = moment().tz("Asia/Seoul");
-  seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
-  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<span>]a[</span>]");
-
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(" h3");
+    let seoulTime = moment().tz("Asia/Seoul");
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<span>]a[</span>]");
+  }
   // PRAGUE
   let pragueElement = document.querySelector("#prague_info");
-  let pragueDateElement = pragueElement.querySelector(".date");
-  let pragueTimeElement = pragueElement.querySelector(" h3");
-  let pragueTime = moment().tz("Europe/Prague");
-  pragueDateElement.innerHTML = pragueTime.format("MMMM Do YYYY");
-  pragueTimeElement.innerHTML = pragueTime.format("h:mm:ss [<span>]a[</span>]");
+  if (pragueElement) {
+    let pragueDateElement = pragueElement.querySelector(".date");
+    let pragueTimeElement = pragueElement.querySelector(" h3");
+    let pragueTime = moment().tz("Europe/Prague");
+    pragueDateElement.innerHTML = pragueTime.format("MMMM Do YYYY");
+    pragueTimeElement.innerHTML = pragueTime.format(
+      "h:mm:ss [<span>]a[</span>]"
+    );
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function zoneSelection(event) {
+  let selectedZone = event.target.value;
+  let selectedZoneDisplay = selectedZone.replace("-", " ").split("/")[1];
+  let selectedTimeZone = moment().tz(selectedZone);
+  let zoneContainer = document.querySelector("#zones_container");
+  zoneContainer.innerHTML = `<div class="zone-div row align-items-center">
+    <div class="col text-start">
+      <h2>${selectedZoneDisplay}</h2>
+      <p class="date">${selectedTimeZone.format("MMMM Do YYYY")}</p>
+    </div>
+    <div class="col">
+      <h3 class="d-flex justify-content-end align-items-start">
+      ${selectedTimeZone.format("h:mm:ss [<span>]a[</span>]")}
+      </h3>
+    </div>
+  </div>`;
+}
+
+let zoneSelectElement = document.querySelector("#zone_select");
+zoneSelectElement.addEventListener("change", zoneSelection);
